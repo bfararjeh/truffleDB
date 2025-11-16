3a21a4ca4974fde74fe27e1726178306b6d9ee7e
Baha Alfararjeh <bahafararjeh@hotmail.com>
2025-11-16 18:34:06 +0000

    Starting on web scraping

--- src/database/run_query.py
+++ src/database/run_query.py
@@ -9,12 +9,15 @@ API_TOKEN = os.getenv("STARTGG_API_TOKEN")
 ENDPOINT = "https://api.start.gg/gql/alpha"
 
 script_dir = Path(__file__).resolve().parent
-query_file = script_dir / "query.gql"
+query_file = script_dir / "GrabAllTournaments.gql"
 
 with open(query_file, "r") as f:
     query = f.read()
 
 variables = {
+    "perPage": 1,
+    "videogameId": 43868,
+    "page": 1
 }
 
 payload = {
@@ -33,5 +36,6 @@ response = requests.post(ENDPOINT, json=payload, headers=headers)
 # Print results (or handle errors)
 if response.status_code == 200:
     print(response.json())
+
 else:
     print(f"Error {response.status_code}: {response.text}")
